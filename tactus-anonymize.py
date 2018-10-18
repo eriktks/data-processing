91bdcb50ec7c1f3927bc15c4e25514d61b66876d
Erik Tjong Kim Sang <erikt@xs4all.nl>
2018-10-18 18:37:43 +0200

    adapting for dutch

--- tactus-anonymize.py
+++ tactus-anonymize.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
     tactus-anonymize.py: anonymize tactus file
-    usage: python3 tactus-anonymyze.py file.xml
+    usage: python3 tactus-anonymyze.py [-l lang] file1.xml [file2.xml ...]
     20181015 erikt(at)xs4all.nl
 """
 
@@ -13,37 +13,65 @@ from xml.etree.ElementTree import parse
 
 COMMAND = sys.argv.pop(0)
 BINDIR = "/home/erikt/projects/e-mental-health/data-processing"
-TARGETTAGS = ["Body","Notes"]
-CLEANTAGS = ["Username","FirstName","LastName","Email","Sender","Recipients"]
+ANONYMIZETAGS = ["Body","Notes"]
+CLEARTAGS = ["Username","FirstName","LastName","Email","Sender","Recipients"]
+CLEARTOKEN = "PERSON"
+
+clearedStringIds = {}
 
 def makeId(fileName):
-    thisId = re.sub(r".*/","",fileName)
-    thisId = re.sub(r"\.xml.*$","",thisId)
-    return(thisId)
+    return(re.sub(r"^(.*/)?(.*)\.xml$","\g<2>",fileName))
+
+def getRootOfFile(inFileName):
+    tree = ET.parse(inFileName)
+    return(tree.getroot())
+
+def getTextId(text):
+    global clearedStringIds
+
+    text = text.lower()
+    if not text in clearedStringIds:
+        clearedStringIds[text] = len(clearedStringIds.keys())
+    return(clearedStringIds[text])
+
+def clearTexts(root,tagNames):
+    for tagname in tagNames:
+        for tag in root.findall(".//"+tagName):
+            tag.text = CLEARTOKEN+"-"+getTextId(tag.text)
+
+def getTextFromXmltext(text):
+    textTree = ET.fromstring("<container>"+text+"</container>")
+    if textTree.text != None: text = textTree.text
+    else: text = ""
+    for node in textTree.findall(".//"):
+        if node.text != None: text += " "+node.text
+    return(text)
+
+def anonymizeTexts(root,tagNames):
+    for tagName in tagNames:
+        for tag in root.findall(".//"+tagName):
+            if tag.text != None:
+                text = getTextFromXmlText(tag.text)
+
+                tmpFile = open("tmpFile","w")
+                print(text,file=tmpFile)
+                tmpFile.close()
+                anonymizeProcess = subprocess.run([BINDIR+"/anonymize-eng.sh","tmpFile"],stdout=subprocess.PIPE)
+                anonymizedText = dict(anonymizeProcess.__dict__)["stdout"]
+                tag.text = anonymizedText.decode("utf8")
+
+def writeFile(tree,outFileName):
+    tree.write(outFileName)
+
 
 def main(argv):
+    clearedStrings = {}
     for inFileName in sys.argv:
-        tree = ET.parse(inFileName)
-        root = tree.getroot()
         thisId = makeId(inFileName)
-        for targetTag in CLEANTAGS:
-            for tag in root.findall(".//"+targetTag):
-                    tag.text = "REMOVED"
-        for targetTag in TARGETTAGS:
-            for tag in root.findall(".//"+targetTag):
-                if tag.text != None:
-                    textTree = ET.fromstring("<container>"+tag.text+"</container>")
-                    if textTree.text != None: text = textTree.text
-                    else: text = ""
-                    for node in textTree.findall(".//"):
-                        if node.text != None: text += " "+node.text
-                    tmpFile = open("tmpFile","w")
-                    print(text,file=tmpFile)
-                    tmpFile.close()
-                    anonymizeProcess = subprocess.run([BINDIR+"/anonymize-eng.sh","tmpFile"],stdout=subprocess.PIPE)
-                    anonymizedText = dict(anonymizeProcess.__dict__)["stdout"]
-                    tag.text = anonymizedText.decode("utf8")
-        tree.write(inFileName+".an")
+        root = getRootFromFile(inFileName)
+        clearTexts(root,CLEARTAGS)
+        anonymizeTexts(root,ANONYMIZETAGS)
+        writeFile(tree,inFileName+".an")
     return(0)
 
 if __name__ == "__main__":
