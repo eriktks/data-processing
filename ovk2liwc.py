7d6fc4a3dda6bc39794be6ae634f0d9ed0abcaba
Erik Tjong Kim Sang <erikt@xs4all.nl>
2019-02-12 17:09:34 +0100

    outputting summarized data

--- ovk2liwc.py
+++ ovk2liwc.py
@@ -10,12 +10,19 @@ import sys
 import importlib
 t2l = importlib.import_module("tactus2liwc-en")
 
+LIWCFILE = "Dutch_LIWC2015_Dictionary.dic"
 CLIENTID = "client-id"
 SENDER = "sender"
 RECIPIENT = "recipient"
 DATE = "date"
 SUBJECT = "subject"
 BODY = "text"
+CLIENTIDID = 0
+SENDERID = 1
+SUBJECTID = 4
+BODYID = 5
+SETSIZE = 3
+METADATA = ["CLIENTID","DATE","NBROFSENTS","Sender","NBROFTOKENS","NBROFMATCHES"]
 
 def readEmailData():
     emails = []
@@ -25,10 +32,66 @@ def readEmailData():
         emails.append([row[CLIENTID],row[SENDER],row[RECIPIENT],row[DATE],row[SUBJECT],row[BODY]])
     return(emails)
 
+def addToSet(set,element,nbrOfUsed):
+    if element[SENDERID] == "CLIENT":
+        if len(set) == 0:
+            set = element
+            nbrOfUsed = 1
+        else:
+            set[SUBJECTID] += " "+ element[SUBJECTID]
+            set[BODYID] += " " + element[BODYID]
+            nbrOfUsed += 1
+    return(set,nbrOfUsed)
+
+def combineData(emails,start,end,setSize,getLast=False):
+    set = []
+    nbrOfUsed = 0
+    if not getLast:
+        i = start
+        while nbrOfUsed < setSize and i <= end:
+            set,nbrOfUsed = addToSet(set,emails[i],nbrOfUsed)
+            i += 1
+    else:
+        i = end
+        while nbrOfUsed < setSize and i >= start:
+            set,nbrOfUsed = addToSet(set, emails[i],nbrOfUsed)
+            i -= 1
+    return(set)
+
+def combineMails(emailsIn):
+    currentId = ""
+    currentStart = -1
+    emailsOut = []
+    for e in range(0,len(emailsIn)):
+        if currentId == "":
+            currentId = emailsIn[e][CLIENTIDID]
+            currentStart = e
+        if currentId != emailsIn[e][CLIENTIDID] or e == len(emailsIn)-1:
+            firstMails = combineData(emailsIn,currentStart,e-1,SETSIZE)
+            lastMails = combineData(emailsIn,currentStart,e-1,SETSIZE,getLast=True)
+            emailsOut.extend([firstMails,lastMails])
+            currentStart = e
+            currentId = emailsIn[e][CLIENTIDID]
+    return(emailsOut)
+
+def printResults(features,results,clientids):
+    allFeatures = METADATA+list(features.keys())
+    csvwriter = csv.DictWriter(sys.stdout,fieldnames=allFeatures)
+    for r in range(0,len(results)):
+        for f in allFeatures: 
+            if not f in results[r] or type(results[r][f]) == type(None): 
+                results[r][f] = ""
+        results[r]["CLIENTID"] = clientids[r]
+        csvwriter.writerow(results[r])
+
 def main(argv):
-    features,words,prefixes = t2l.readLiwcDict(t2l.LIWCDIR+t2l.LIWCFILE)
+    features,words,prefixes = t2l.readLiwcDict(t2l.LIWCDIR+LIWCFILE)
     emails = readEmailData()
-    if len(emails) > 0: t2l.emails2liwc(emails,features,words,prefixes)
+    emailsCombined = combineMails(emails)
+    if len(emailsCombined) > 0: 
+        results = t2l.emails2liwc(emailsCombined,features,words,prefixes)
+        clientids = [e[CLIENTIDID] for e in emailsCombined]
+        printResults(features,results,clientids)
     return(0)
 
 if __name__ == "__main__":
