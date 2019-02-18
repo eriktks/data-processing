2d47eca5d38c5df4261843f4ce833713e891952b
Erik Tjong Kim Sang <erikt@xs4all.nl>
2019-02-18 18:43:31 +0100

    adapted to new data format

--- table2wide.py
+++ table2wide.py
@@ -9,44 +9,43 @@
 import csv
 import sys
 
-COMBINE = ["cesd","mhc","avgSentLenCli","avgWordLenCli"]
 ID = "id"
-KEEP = ["id","treatment","counselor","avgSentLenCouns","avgWordLenCouns"]
+EMPTYDICT = {}
+KEEP = [ID,"treatment","counselor"]
 SEPARATOR = ","
-TIME = "time"
+TIME = "timeframe"
 TIMES = ["T0","T1"]
 
-def makeNewFieldNames():
-    fieldNames = list(KEEP)
-    for field in COMBINE:
-        for time in TIMES:
-            fieldNames.append(field+time)
-    return(fieldNames)
+def makeNewFieldNames(fieldNamesIn):
+    fieldNamesOut = list(KEEP)
+    for field in fieldNamesIn:
+        if not field in KEEP and field != TIME:
+            for time in TIMES:
+                fieldNamesOut.append(field+time)
+    return(fieldNamesOut)
+
+def fillWithNA(fieldNames):
+    return({ f:"NA" for f in fieldNames })
 
 def main(argv):
-    csvReader = csv.DictReader(sys.stdin,delimiter=SEPARATOR)
-    fieldNames = makeNewFieldNames()
-    # do not use csvWriter: it generates invisible line-end characters
-    for i in range(0,len(fieldNames)): 
-        if i > 0: print(SEPARATOR,end="")
-        print(fieldNames[i],end="")
-    print()
-    lastRow = {"id":"" }
-    for row in csvReader:
-        if lastRow["id"] == "":
+    csvreader = csv.DictReader(sys.stdin,delimiter=SEPARATOR)
+    fieldNames = makeNewFieldNames(csvreader.fieldnames)
+    csvwriter = csv.DictWriter(sys.stdout,delimiter=SEPARATOR,fieldnames=fieldNames)
+    csvwriter.writeheader()
+    lastRow = EMPTYDICT
+    for row in csvreader:
+        if lastRow == EMPTYDICT:
             lastRow = row
-            row = csvReader.__next__()
-        outRow = {}
-        for field in KEEP: outRow[field] = lastRow[field].replace("\r","")
-        for field in COMBINE:
-            outRow[field+lastRow[TIME]] = lastRow[field].replace("\r","")
-            if row[ID] == lastRow[ID]: outRow[field+row[TIME]] = row[field].replace("\r","")
-        for i in range(0,len(fieldNames)):
-            if i > 0: print(SEPARATOR,end="")
-            print(outRow[fieldNames[i]],end="")
-        print()
-        if row[ID] != lastRow[ID]: lastRow = row
-        else: lastRow = { "id":"" }
+            row = csvreader.__next__()
+        outRow = fillWithNA(fieldNames)
+        for field in KEEP: outRow[field] = lastRow[field]
+        for field in row.keys():
+            if not field in KEEP and field != TIME:
+                outRow[field+lastRow[TIME]] = lastRow[field]
+                if row[ID] == lastRow[ID]: outRow[field+row[TIME]] = row[field]
+        csvwriter.writerow(outRow)
+        if row[ID] == lastRow[ID]: lastRow = EMPTYDICT
+        else: lastRow = row
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
