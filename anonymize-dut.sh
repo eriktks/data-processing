5beced6b8f67909725e9cf3ae91b170d3e00a82f
Erik Tjong Kim Sang <erikt@xs4all.nl>
2019-04-09 14:43:47 +0200

    linked to anonymize-ovk.py

--- anonymize-dut.sh
+++ anonymize-dut.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # anonymize-dut.sh: anonymize Dutch text
-# usage: anonymize-dut.sh < file.txt
+# usage: anonymize-dut.sh file.txt
 # 20181018 erikt(at)xs4all.nl
 
 BINDIR="/home/erikt/projects/e-mental-health/data-processing"
+INFILE="$1"
 
-source activate env
-python3 $BINDIR/ner-frog.py |\
-   python3 $BINDIR/anonymize-dut.py
+source activate python
+python3 $BINDIR/ner-frog.py < $INFILE |\
+   python3 $BINDIR/anonymize-ovk.py
 
 exit 0
