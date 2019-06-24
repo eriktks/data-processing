c552dc1f2f1ce8e7ac180d8dce7b5e6adf37cba0
Erik Tjong Kim Sang <erikt@xs4all.nl>
2019-06-24 18:45:36 +0200

    updated

--- tactus-anonymize.py
+++ tactus-anonymize.py
@@ -6,6 +6,7 @@
 """
 
 import html
+import os
 import re
 import subprocess
 import sys
@@ -15,31 +16,44 @@ COMMAND = sys.argv.pop(0)
 BINDIR = "/home/erikt/projects/e-mental-health/data-processing"
 ANONYMIZEPRG = "anonymize-dut.sh"
 ANONYMIZETAGS = ["Body","Subject", "Notes"]
-CLEARTAGS = ["Username","FirstName","LastName","Email","Sender","Recipients"]
-CLEARTOKEN = "PERSON"
+CLEARTAGS = ["AssignedCounselor","Intake","Treatment","Diary","Sender","Recipients"]
+CLEARTOKEN = "REMOVED"
+CLEAREDSTRINGIDS = {"client":"CLIENT"}
+OUTFILESUFFIX = "-an"
+TMPFILENAME = "tactus-anonymize.py."+str(os.getpid())
 
-clearedStringIds = {}
-
-def makeId(fileName):
-    return(re.sub(r"^(.*/)?(.*)\.xml$","\g<2>",fileName))
+clearedStringIds = CLEAREDSTRINGIDS
 
 def getRootOfFile(inFileName):
     return(ET.parse(inFileName))
 
-def getTextId(text):
+def normalizeWhiteSpace(string):
+    string = re.sub("^\s+","",string)
+    string = re.sub("\s+$","",string)
+    string = re.sub("\s+"," ",string)
+    return(string)
+
+def removeTagText(tag):
     global clearedStringIds
 
-    if text is None: text = ""
-    text = text.lower()
-    if not text in clearedStringIds:
-        clearedStringIds[text] = str(len(clearedStringIds.keys()))
-    return(clearedStringIds[text])
+    if not tag.text is None:
+        tag.text = normalizeWhiteSpace(tag.text)
+        if tag.text != "":
+            text = tag.text.lower()
+            if not text in clearedStringIds:
+                clearedStringIds[text] = CLEARTOKEN+"-"+str(len(clearedStringIds.keys()))
+            tag.text = clearedStringIds[text]
+
+def removeTagChildren(tag):
+    for child in [c for c in tag]:   
+        tag.remove(child)
 
 def clearTexts(tree,tagNames):
     root = tree.getroot()
     for tagName in tagNames:
-        for tag in root.findall(".//"+tagName):
-            tag.text = CLEARTOKEN+"-"+getTextId(tag.text)
+       for tag in root.findall(".//"+tagName):
+           removeTagText(tag)
+           removeTagChildren(tag)
 
 def getTextFromXmlText(text):
     try: textTree = ET.fromstring("<container>"+text+"</container>")
@@ -50,29 +64,45 @@ def getTextFromXmlText(text):
         if not node.text is None: text += " "+node.text
     return(text)
 
+def runAnonymizeProcess(inText):
+    text = getTextFromXmlText(html.escape(inText))
+    tmpFile = open(TMPFILENAME,"w")
+    print(text,file=tmpFile)
+    tmpFile.close()
+    anonymizeProcess = subprocess.run([BINDIR+"/"+ANONYMIZEPRG,TMPFILENAME],stdout=subprocess.PIPE)
+    anonymizedText = dict(anonymizeProcess.__dict__)["stdout"]
+    outText = anonymizedText.decode("utf8")
+    os.remove(TMPFILENAME)
+    return(outText)
+
 def anonymizeTexts(tree,tagNames):
     root = tree.getroot()
     for tag in root.iter():
         if not tag.text is None:
             if tag.tag in tagNames:
                 text = getTextFromXmlText(html.escape(tag.text))
-                tmpFile = open("tmpFile","w")
-                print(text,file=tmpFile)
-                tmpFile.close()
-                anonymizeProcess = subprocess.run([BINDIR+"/"+ANONYMIZEPRG,"tmpFile"],stdout=subprocess.PIPE)
-                anonymizedText = dict(anonymizeProcess.__dict__)["stdout"]
-                tag.text = anonymizedText.decode("utf8")
+                tag.text = runAnonymizeProcess(text)
             tag.text = html.unescape(tag.text)
+            tag.text = re.sub(r"\n+\s*$","",tag.text)
 
 def writeFile(tree,outFileName):
     tree.write(outFileName,encoding="utf8")
 
+def makeOutFileName(fileName):
+    parts = fileName.split(".")
+    if len(parts) > 0: parts[-2] += OUTFILESUFFIX
+    fileName = ".".join(parts)
+    return(fileName)
+
 def main(argv):
+    global clearedStringIds
+
     for inFileName in argv:
+        clearedStringIds = CLEAREDSTRINGIDS
         tree = getRootOfFile(inFileName)
         clearTexts(tree,CLEARTAGS)
         anonymizeTexts(tree,ANONYMIZETAGS)
-        writeFile(tree,inFileName+".an")
+        writeFile(tree,makeOutFileName(inFileName))
     return(0)
 
 if __name__ == "__main__":
