a7b47ce5c603ee48572a5484778dbebb8db6bc37
Erik Tjong Kim Sang <erikt@xs4all.nl>
2019-12-03 18:21:22 +0100

    included requested combis

--- combineColmns.py
+++ combineColmns.py
@@ -10,10 +10,11 @@ import re
 import sys
 
 EMPTY = "EMPTY"
+REQUESTED = {"8-opleidng0":"4-opleidng","12-aanleid0":"7-aanleid","25-behdrinkt0":"19-behdrink"}
 EXCEPTIONS = [ "14-drugs0","14-drugs1","14-drugs2","14-drugs3","14-drugs4","14-drugs5","25-rcq10","25-rcq11","25-rcq12","22-medi00","31-dass100","31-dass200","1-medi0n","1-medi1n","34-medi02","34-medi12","34-medi22","34-medi32","34-medi42","34-medi52","38-mateicn100","37-dass122","38-dass122","41-mateicn122","42-mateicn122" ]
 
 def findDuplicateColumns(columnNames):
-    duplicateColumns = {}
+    duplicateColumns = REQUESTED
     for columnName in columnNames:
         shortName = re.sub(r"..$",r"",columnName)
         if shortName in columnNames and not columnName in EXCEPTIONS:
@@ -42,6 +43,7 @@ def main(argv):
                     row[duplicateColumns[key]] = row[key]
                 else:
                     print("clash",key,row[key],duplicateColumns[key],row[duplicateColumns[key]],file=sys.stderr)
+                    row[duplicateColumns[key]] += "#"+row[key]
         outRow = {key:row[key] for key in row if not key in duplicateColumns}
         csvwriter.writerow(outRow)
 
